5f94e7772102b14b9aa2327cee63b112887adebb
SCHAC <kouakouchristevann@gmail.com>
2026-09-09 10:08:57 +0000

    Update large_conference_screen.dart

--- lib/screens/large_conference_screen.dart
+++ lib/screens/large_conference_screen.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:ui' show ImageFilter;
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:flutter/services.dart' show PlatformException;
+import 'package:flutter/services.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
